46536a9aa57f1c20f667bb2e5ef151d9151c045c
Matheus Victor <matheusvictor.salles@gmail.com>
2020-11-06 11:28:06 -0300

    Add nova tentativa de resolução da questão5.cpp

--- lab-de-prog_sls-2020/lista4/questao5.cpp
+++ lab-de-prog_sls-2020/lista4/questao5.cpp
@@ -4,14 +4,33 @@
 using namespace std;
 
 int main(){
-    int qtd_testes, numero, fibNumero;
+    
+    int qtd_testes, maiorNumero = 0;
     cin >> qtd_testes;
+    int vetorNumeros[qtd_testes];
 
-    for(int i=0; i < qtd_testes; i++){
-      cin >> numero;
-      int fib[numero];
+    //guardando todos os números digitados em um vetor
+    for(int i = 0; i < qtd_testes; i++){
+      cin >> vetorNumeros[i];
+      //guardando o maior número digitado em uma variável
+      if(i == 0){
+        maiorNumero = vetorNumeros[0];
+      } else{
+        if(vetorNumeros[i] > maiorNumero){
+          maiorNumero = vetorNumeros[i];
+        }
+      }
+    }
+    
+    //imprimindo o teste de maior número
+    //cout << "Maior nº:" << maiorNumero << endl;
 
-      for(int j = 0; j <= numero; j++){
+    //imprimindo o vetor dos números armazenados
+    //cout << "Numeros armazenados:" << vetorNumeros[0] <<  vetorNumeros[1] <<  vetorNumeros[2] << endl;
+
+    int fib[maiorNumero];
+        
+    for(int j = 0; j <= maiorNumero; j++){
         if(j == 0){
           fib[0] = 0;
         } else if(j == 1){
@@ -19,8 +38,12 @@ int main(){
         } else{
           fib[j] = fib[j-1] + fib[j-2];
         }
-      }
-      cout << "Fib(" << numero << ") = " << fib[numero] << endl;
+        //imprimindo tds os fibs, de 0 até o maiorNumero
+        //cout << endl << "FIB" << fib[j] << endl;
+    }
+   
+    for(int k = 0; k < qtd_testes; k++){
+      cout << "Fib(" << vetorNumeros[k] << ") = " << fib[vetorNumeros[k]] << endl;
     }
     return 0;
 }
